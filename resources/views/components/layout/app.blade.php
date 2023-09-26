5ee1ab6a1db3cfc353a174db11bfe210e530a40c
UVallejos <ulises14.2@gmail.com>
2023-09-26 21:02:08 +0200

    Quitar contacto

--- resources/views/components/layout/app.blade.php
+++ resources/views/components/layout/app.blade.php
@@ -222,10 +222,10 @@
 
 
 <!--Contacto-->
-<div id="contact" class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-10 drop-shadow-xl">
+<!--<div id="contact" class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-10 drop-shadow-xl">-->
   
 <!--  <div class="grid gap-8 lg:grid-cols-1 sm:max-w-sm sm:mx-auto lg:max-w-full">-->
-    <!--<x-contact></x-contact>-->
+    
 <!--</div>-->
 <!--</div>-->
 
